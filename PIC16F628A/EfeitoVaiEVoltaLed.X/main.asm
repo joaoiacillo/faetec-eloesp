f4abc2843728608c3714a29a7a629ee906eb7577
João Pedro <joaopiacillo@outlook.com.br>
2022-08-28 08:48:12 -0300

    Added PIC16F628A

---
+++ PIC16F628A/EfeitoVaiEVoltaLed.X/main.asm
@@ -0,0 +1,113 @@
+#INCLUDE <P16F628A.INC>
+	__CONFIG  _BODEN_ON & _CP_OFF & _PWRTE_ON & _WDT_OFF & _LVP_OFF & _MCLRE_ON & _XT_OSC
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Banks					;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+#DEFINE	BANK0	BCF STATUS, RP0
+#DEFINE	BANK1	BSF STATUS, RP0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Inputs					;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
+	
+#DEFINE RUNBTN PORTA, 1  ; Starts the effect once pressed
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Constants					;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+#DEFINE INITIAL B'00000001' ; The initial led formation once run btn is pressed
+#DEFINE	MAX	0x03
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Variables					;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+DCounter1 EQU 0X0C
+DCounter2 EQU 0X0D
+DCounter3 EQU 0X0E
+ 
+CBLOCK	0x20
+	REPEAT
+ENDC
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Reset Vector				;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ 
+	ORG	0x00
+	GOTO	START
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Delay					;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+DELAY
+	MOVLW 0X5c
+	MOVWF DCounter1
+	MOVLW 0X26
+	MOVWF DCounter2
+	MOVLW 0X0b
+	MOVWF DCounter3
+LOOP
+	DECFSZ DCounter1, 1
+	GOTO LOOP
+	DECFSZ DCounter2, 1
+	GOTO LOOP
+	DECFSZ DCounter3, 1
+	GOTO LOOP
+	NOP
+	RETURN
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Program					;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+START
+	BANK1
+	CLRF	TRISB
+	MOVLW	B'00000010'  ; Bit 1 -> Input
+	MOVWF	TRISA
+	BANK0
+	
+MAIN	
+	; The mcu is useless until the run button is pressed.
+	; That's when the magic happens!
+	BTFSC	RUNBTN
+	GOTO	MAIN
+	
+	MOVLW	INITIAL
+	MOVWF	PORTB
+	
+	MOVLW	MAX
+	MOVWF	REPEAT
+	
+SHIFTL  ; Shifiting 1 bit to the left.
+	; This is the same as adding PORTB to PORTB.
+	CALL	DELAY
+	RLF	PORTB
+	DECFSZ	REPEAT
+	GOTO	SHIFTL
+	
+	MOVLW	MAX
+	MOVWF	REPEAT
+	
+SHIFTR	; Shifting 1 bit to the right.
+	; This is the same as PORTB / 2
+	CALL	DELAY
+	RRF	PORTB
+	DECFSZ	REPEAT
+	GOTO	SHIFTR
+	
+	; The moment everyone's been waiting for...
+	; The end of the effect when everything is turned
+	; off.
+	CALL	DELAY
+	CLRF	PORTB
+	
+	
+	GOTO MAIN
+	END
